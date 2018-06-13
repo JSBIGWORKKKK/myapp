3fb898bf5d038c79e84720baa6bcbd3cf782a4b9
yjc9696 <32888676+yjc9696@users.noreply.github.com>
2018-06-13 16:50:23 +0800

    JS后台修改（PUT）

--- app.js
+++ app.js
@@ -4,19 +4,23 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var sqlRouter=require('./routes/sql');
 var app = express();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 //sql();
+app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'mzc_html')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/sql',sqlRouter);
--- routes/sql.js
+++ routes/sql.js
@@ -12,33 +12,39 @@ router.get(/\w*/, function(req, res, next) {
 
 	connection.connect();
 	console.log(req.query.val);
-	if(req.query.val) {
-		console.log("hava val");
-		var qq = "UPDATE seats.work SET seats = " + req.query.val + "  WHERE place =" + req.query.name;
-		console.log(qq);
-		var ans;
-		connection.query(qq, function(err, rows, fields) {
-			if(err) throw err;
-			//ans = new String(rows[0].seats);
-			//console.log('The solution is: ', ans);
-			res.send("succerss");
+	//var qq=["a","b"].join('');
+	var qq = "SELECT `seats` FROM movie.data where performance='" + req.query.name+"'";
+	console.log(qq);
+	var ans;
+	connection.query(qq, function(err, rows, fields) {
+		if(err) throw err;
+		ans = new String(rows[0].seats);
+		console.log('The solution is: ', ans);
+		res.send(ans);
 
-		});
-	} else {
-		//var qq=["a","b"].join('');
-		var qq = "SELECT `seats` FROM seats.`work` where place=" + req.query.name;
-		console.log(qq);
-		var ans;
-		connection.query(qq, function(err, rows, fields) {
-			if(err) throw err;
-			ans = new String(rows[0].seats);
-			console.log('The solution is: ', ans);
-			res.send(ans);
-
-		});
-	}
+	});
 	connection.end();
 
 });
+router.put(/\w*/, function(req, res, next) {
+	var mysql = require('mysql');
+	var connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: '123456'
+	});
+	connection.connect();
+	//console.log(req);
+	console.log(req.body.name);
+	console.log(req.body.val);
+	var qq = "UPDATE movie.data SET seats = " + req.body.val + "  WHERE performance ='" + req.body.name+"'";
+	console.log(qq);
+	connection.query(qq, function(err, rows, fields) {
+		if(err) throw err;
+		
 
+	});
+	res.send("success")
+	connection.end();
+});
 module.exports = router;
